dea8a3dce418d2711944768d908f09de1289709a
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-09-12 17:04:37 +0200

    feat:piechart on homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NgxChartsModule],
   providers: [
-    provideCharts(withDefaultRegisterables()),
     provideHttpClient()
   ],
   bootstrap: [AppComponent],
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PieChartComponent } from '@swimlane/ngx-charts';
 import { Observable, of } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic.interface';
+import { Participation } from 'src/app/core/models/Participation.interface';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -13,7 +15,29 @@ export class HomeComponent implements OnInit {
 
   constructor(private olympicService: OlympicService) {}
 
+  chartData:Array<Object> = [];
+
+  updateChart(olympics: Olympic[]) :void {
+    this.chartData = olympics.map(
+      (olympic: Olympic) => {
+        return {
+          name: olympic.country, 
+          value: olympic.participations.reduce((total, participation: Participation) => {return total + participation.medalsCount}, 0), 
+          extra: {id: olympic.id}
+        }
+      }
+    );
+  }
+
+  onChartClick(event?: any) :void {
+    console.log('navigate to '+event.extra.id);
+  }
+  
+
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.olympics$.subscribe((res) => {
+      this.updateChart(res);
+    });
   }
 }
