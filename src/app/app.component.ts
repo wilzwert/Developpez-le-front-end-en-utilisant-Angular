72401d22e49fdc62cd0bc2a574d8f5ee96a34741
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-10-04 13:35:11 +0200

    feat:add loading message on app initial load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,6 @@ export class AppComponent implements OnInit {
    */
   ngOnInit(): void {
     this.olympicService.loadInitialData().subscribe();
+    document.body.classList.remove('app-loading');
   }
 }
